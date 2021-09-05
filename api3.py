603fc85ffb0845adfeea91e529015137be0b662b
MasayaOnishi305 <72341237+MasayaOnishi305@users.noreply.github.com>
2021-09-05 14:50:50 +0900

    確認後修正

--- api3.py
+++ api3.py
@@ -4,15 +4,20 @@ import pandas as pd
 
 CSV_FILE = "stock.csv"
 
-def get_api(url):
-    result = requests.get(url)
+def get_api(url,params):
+    result = requests.get(url,params=params)
+    if not(300 > result.status_code >= 200):
+        print("api error")
+        return None 
     return result.json()
 
 def main():
-    genreId="100283"
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?applicationId=1013826913582053233&genreId={}".format(
-        genreId)
-    resp = (get_api(url))
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628"
+    params = {
+        "applicationId":1013826913582053233,
+        "genreId":100283
+    }  
+    resp = (get_api(url,params))
     title = resp['title']
     date = resp['lastBuildDate']
     exp_rank_list = []
@@ -21,9 +26,9 @@ def main():
     print (f"【ジャンル】{title} \n【取得日】{date}\n ===================================")
     for i in resp['Items']:
         item = i['Item']
-        exp_rank_list.append({item['rank']})
-        exp_name_list.append({item['itemName']})
-        exp_price_list.append({item['itemPrice']})
+        exp_rank_list.append(item['rank'])
+        exp_name_list.append(item['itemName'])
+        exp_price_list.append(item['itemPrice'])
     
     #CSV出力
     # DataFrameに対して辞書形式でデータを追加する
@@ -33,10 +38,6 @@ def main():
             "ランク": exp_rank_list,
             "商品名": exp_name_list,
             "金額": exp_price_list})
-        #データの整形
-        # df["ランク"] = df["ランク"].replace("{","")
-        # df["商品名"] = df["商品名"].str.replace("{'","").str.replace("'}","")
-        # df["金額"] = df["金額"].replace("{'","").replace("'}","")
         df.to_csv(CSV_FILE,encoding="UTF_8_sig")
         return df
     except Exception as e:
